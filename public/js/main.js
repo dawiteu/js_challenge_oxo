53ed84efd40f77a4a94beb7a6529ae4d168c3c5d
dawiteu <dawid@tararuj.be>
2021-02-17 10:43:36 +0100

    99 %

--- public/js/main.js
+++ public/js/main.js
@@ -3,16 +3,68 @@ let jouer = document.querySelector("#joueur");
 
 let joueurAct = false; // X  
             //=  true; // O 
+let enjeu = true; // on commence le jeu en jouant; 
+
+
+let tabJeu = []; 
+
+
+//let tabJeu = ["","","","","","","",""]; 
+
+
+const gagneCond = [ 
+        [0, 1, 2], 
+        [3, 4, 5], 
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+];
+
+
+let check = () => {
+    
+    for(let i=0; i <= 7; i++){
+        let checkPos = gagneCond[i]; 
+        let posA = tabJeu[checkPos[0]];
+        let posB = tabJeu[checkPos[1]];
+        let posC = tabJeu[checkPos[2]]; 
+
+        if(posA != undefined && posB != undefined && posC != undefined){
+            if(posA == posB && posB == posC){
+                console.log('gagné!');
+                jouer.parentElement.innerHTML="On a un gagnant! C'est : "+posC; 
+                enjeu=false; // on sort du jeu; 
+            }
+        }
+    }
+}
+
+
 cols.forEach((e) =>{
-    e.addEventListener("click", () => {
-        if (joueurAct == true) {
-            e.innerHTML = "O";
-            jouer.innerHTML= "X";
-            joueurAct=false;
-        }else{
-            e.innerHTML = "X";
-            jouer.innerHTML= "O";
-            joueurAct=true; 
+    e.addEventListener("click", () => { 
+        if(enjeu){
+            let pos = e.getAttribute('id').substring(1); 
+            check();
+            if (joueurAct == true) { 
+                if(e.innerHTML == ""){
+                    e.innerHTML = "O";
+                    tabJeu[pos-1] = "O"; 
+                    jouer.innerHTML= "X";
+                    joueurAct=false;
+                    check();
+                }
+            }else{
+                if(e.innerHTML == ""){
+                    e.innerHTML = "X";
+                    jouer.innerHTML= "O";
+                    tabJeu[pos-1] = "X"; 
+                    joueurAct=true; 
+                    check();
+                }
+            }     
         }
     });
 })
